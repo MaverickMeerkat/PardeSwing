8149812fc9553937464ba00882e252c77bfc8cb8
MaverickMeerkat <davidrefaeli@gmail.com>
2026-04-14 13:30:31 +0300

    image fix

--- src/pardeswing_landing_page.jsx
+++ src/pardeswing_landing_page.jsx
@@ -19,7 +19,7 @@ export default function PardeSwingLandingPage() {
 
       <section className="relative min-h-[110vh] flex items-start justify-center">
         <img
-          src="../public/Hero.jpg"
+          src="Hero.jpg"
           alt="Swing dancing"
           className="absolute inset-0 w-full h-full object-cover object-[center_80%]"
         />
@@ -129,7 +129,7 @@ export default function PardeSwingLandingPage() {
       
       {/* Circle image */}
       <img
-        src="../public/Me.jpg" // ../public/
+        src="Me.jpg"
         alt="David"
 className="w-52 h-52 md:w-64 md:h-64 rounded-full object-cover border-4 border-[#e5e6b5] shadow-lg mx-auto"      />
 
